e18f35a2a8d38b1750579788b31d59142c7ad113
Arianna Zhang <59743164+ariannazhangg@users.noreply.github.com>
2021-09-20 10:02:09 -0700

    Add files via upload

---
+++ Problem Set 1-A/Slope.java
@@ -0,0 +1,8 @@
+class Slope {
+    public static void main (String [] args) {
+        
+        
+        //system print
+        System.out.println ("A line connecting the points (" + "x1" );
+    }
+}
